40c32b648f3b61eaa903fec74d928c70d76cece7
darya <lukina-dasha@mail.ru>
2024-08-06 12:46:35 +0300

    babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -74,13 +74,34 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
       }
     }
   ]
-  }
+  };
+
+  const babelLoader = {
+    test: /\.tsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-typescript',
+            [
+              '@babel/preset-react',
+              {
+                runtime: isDev ? 'automatic' : 'classic'
+              }
+            ]
+          ]
+        }
+    }
+  };
 
   return [
     assetLoader,
     svgrLoaders,
     // порядок важен
     scssLoader,
-    tsLoader,
+    // tsLoader,
+    babelLoader
   ];
 }
